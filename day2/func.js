a06612c648aa67985dcd1bb908fdf3506b001a02
Naveen <snaveenprakash06@gmail.com>
2025-02-06 10:45:10 +0530

    test

--- day2/func.js
+++ day2/func.js
@@ -1,141 +1,136 @@
-/*const person={
-    name:"Naveen",
-    age:21,
-}; /// This is an object
+// const person={
+//     name:"Naveen",
+//     age:21,
+// }; /// This is an object
 
-function getDetails(person){
-    console.log(person.age); // object called with the attribute also
-    console.log(person.name);
-}
+// function getDetails(person){
+//     console.log(person.age); // object called with the attribute also
+//     console.log(person.name);
+// }
 
-getDetails(person); /// calling the function
-getDetails({name:"Haii", age:23}); // creating object in the function argument
+// getDetails(person); /// calling the function
+// getDetails({name:"Haii", age:23}); // creating object in the function argument
 
+// /*------------------------------------------------------------------------------------------------------------*/
 
-/*------------------------------------------------------------------------------------------------------------*/
+// const person1={
+//     name:"Prakash",
+//     age:24,
+// }; 
 
+// function printP1(name){ // parameter
+//     console.log(name);
+// }
 
-const person1={
-    name:"Prakash",
-    age:24,
-}; 
+// printP1(person1.name); /// calling the function /// aruguments
 
-function printP1(name){ // parameter
-    console.log(name);
-}
+// /*------------------------------------------------------------------------------------------------------------*/
 
-printP1(person1.name); /// calling the function /// aruguments
+// const person2={
+//     name:"Prakashi",
+//     age:24,
+// }; 
 
-/*------------------------------------------------------------------------------------------------------------*/
+// function printP2(name=" Durga "){ // parameter
+//     console.log(name);
+// }
 
-const person2={
-    name:"Prakashi",
-    age:24,
-}; 
+// printP2(person2.name);
+// /*------------------------------------------------------------------------------------------------------------*/
 
-function printP2(name=" Durga "){ // parameter
-    console.log(name);
-}
+// let email= Symbol(); // to hide the keys 
+// let employee = {};
+//     employee.name="Name",
+//     employee.age=21;
+// employee[email]="Prakash@test.com";
+// console.log(Object.keys(employee)); // printing the keys 
+// console.log(employee[email]);  // only way to print the hide key 
 
-printP2(person2.name);
-/*------------------------------------------------------------------------------------------------------------*/
+// /*------------------------------------------------------------------------------------------------------------*/
 
-let email= Symbol(); // to hide the keys 
-let employee = {};
-    employee.name="Name",
-    employee.age=21;
-employee[email]="Prakash@test.com";
-console.log(Object.keys(employee)); // printing the keys 
-console.log(employee[email]);  // only way to print the hide key 
+// function sayHello(){        // function decalartion
+// 	console.log("hello "); 
+// };
+// sayHello();
 
+// const greeting= function sayHello(){      // Function expression or Named function expression
+// 	console.log("hello from greeting");
+// };
+// greeting();
 
-/*------------------------------------------------------------------------------------------------------------*/
+// const AnonFunc= function (){      //Anonymous Function 
+// 	console.log("hello from Anonymous");
+// };
+// AnonFunc();
 
-function sayHello(){        // function decalartion
-	console.log("hello "); 
-};
-sayHello();
+// const greet = () => {   // arrow function or fat arrow fucntion  
+//     console.log("Hi customer")
+// }
+// greet();
 
-const greeting= function sayHello(){      // Function expression or Named function expression
-	console.log("hello from greeting");
-};
-greeting();
+// // passing with one argument
+// const greeti = (x) => { // const greeti = x => {  
+//     return x*x*x;
+// }
 
-const AnonFunc= function (){      //Anonymous Function 
-	console.log("hello from Anonymous");
-};
-AnonFunc();
+// let result = greeti(4);
+// console.log(result);
 
-const greet = () => {   // arrow function or fat arrow fucntion  
-    console.log("Hi customer")
-}
-greet();
+// /*------------------------------------------------------------------------------------------------------------*/
 
+// // Arrow Function in Object
+// const employee6 ={
+//     firstname: 'Arun',
+//     lastname: 'Kumar1',
+//     // getFullname(){ ----> NORMAL METHOD
+//     //     return this.firstname + this.lastname;
+//     // }
+//     getFullname:()=>{
+//         return employee6.firstname+employee6.lastname
+//     }
+// }
 
-// passing with one argument
-const greeti = (x) => { // const greeti = x => {  
-    return x*x*x;
-}
+// console.log(employee6.getFullname());
 
-let result = greeti(4);
-console.log(result);
-/*------------------------------------------------------------------------------------------------------------*/
+// alert("Hello da")
 
-// Arrow Function in Object
-const employee6 ={
-    firstname: 'Arun',
-    lastname: 'Kumar1',
-    // getFullname(){ ----> NORMAL METHOD
-    //     return this.firstname + this.lastname;
-    // }
-    getFullname:()=>{
-        return employee6.firstname+employee6.lastname
-    }
-}
+// let cnf = confirm ("Are you sure? ");
+// if(cnf ){
+//     alert("Success");
+// }
+// else{
+//     alert("Sorry");
+// }
 
-console.log(employee6.getFullname());
+// let age = prompt("Enter your age; ");
+// if(age>18){
+//     alert("good");
+// }
+// else{
+//     alert("bad");
+// }
 
+// function points(argument){
+//     console.log("Running");
+//     const total =argument;
 
-alert("Hello da")
+//     // Add, remove and getTotal
+//     const addPoint = () => {
+//         return total + 1;
+//     }
 
+//     const removePoint = (points) => {
+//         return total - points;
+//     }
 
-let cnf = confirm ("Are you sure? ");
-if(cnf ){
-    alert("Success");
-}
-else{
-    alert("Sorry");
-}
+//     const getTotalPoint = () => {
+//         return total;
+//     }
+//     return {addPoint, removePoint, getTotalPoint}
 
-let age = prompt("Enter your age; ");
-if(age>18){
-    alert("good");
-}
-else{
-    alert("bad");
-}*/
+//     }
 
-function points(argument){
-    console.log("Running");
-    const total =argument;
-
-    // Add, remove and getTotal
-    const addPoint = () => {
-        return total + 1;
-    }
-
-    const removePoint = (points) => {
-        return total - points;
-    }
-
-    const getTotalPoint = () => {
-        return total;
-    }
-    return {addPoint, removePoint, getTotalPoint}
-
-    }
-
-const table = points(100);
-console.log(table.addPoint());
-console.log(table.removePoint(10));
+// const table = points(100);
+// console.log(table.addPoint());
+// console.log(table.removePoint(10));
  
---
+++ day3/script.js
@@ -0,0 +1,78 @@
+// script.js
+const createCalculator = () => {
+    let lastResult = 0;
+
+    const add = (a, b) => a + b;
+    const subtract = (a, b) => a - b;
+    const multiply = (a, b) => a * b;
+    const divide = (a, b) => (b !== 0 ? a / b : "Error: Division by zero");
+
+    const validateInput = (input) => !isNaN(parseFloat(input)) && input !== ""; // Parse float for decimals
+
+    const updateLastResult = (result) => {
+        lastResult = result;
+    };
+
+    const getLastResult = () => lastResult;
+
+    return {
+        add,
+        subtract,
+        multiply,
+        divide,
+        validateInput,
+        updateLastResult,
+        getLastResult,
+    };
+};
+
+const calculator = createCalculator();
+
+const display = document.getElementById("display");
+
+const appendToDisplay = (value) => {
+    display.value += value;
+};
+
+const clearDisplay = () => {
+    display.value = "";
+};
+
+const calculate = () => {
+    const expression = display.value;
+
+    try {
+        // Use eval() for simpler calculation (with security considerations)
+        const result = eval(expression);  // Be cautious using eval in production.
+        if (calculator.validateInput(result)) { // Validate the *result*
+          display.value = result;
+          calculator.updateLastResult(result);
+        } else {
+          display.value = "Error: Invalid Calculation";
+        }
+
+    } catch (error) {
+        display.value = "Error: Invalid Expression";
+        console.error("Calculation Error:", error); // Log the error for debugging
+    }
+};
+
+
+// Button event listeners (Improved)
+const buttons = document.querySelectorAll('.buttons button'); // More specific selector
+
+buttons.forEach(button => {
+    button.addEventListener('click', () => {
+        const buttonValue = button.textContent;
+        switch (buttonValue) {
+            case '=':
+                calculate();
+                break;
+            case 'C':
+                clearDisplay();
+                break;
+            default:
+                appendToDisplay(buttonValue);
+        }
+    });
+});
