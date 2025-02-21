79b4a4ee7ea3a06512145cc24efc21b6cb045d10
Naveen <snaveenprakash06@gmail.com>
2025-02-21 16:55:26 +0530

    test

---
+++ day15(21)/script.js
@@ -0,0 +1,34 @@
+// function pow(x, n) {
+//   if (n == 1) {
+//     return x;
+//   } 
+//   else {
+//     return x * pow(x, n - 1);
+//   }
+// }
+  
+
+// Recursive Traversallet company = {
+  sales: [{
+    name: 'John',
+    salary: 1000
+  }, {
+    name: 'Alice',
+    salary: 1600
+  }],
+
+  development: {
+    sites: [{
+      name: 'Peter',
+      salary: 2000
+    }, {
+      name: 'Alex',
+      salary: 1800
+    }],
+
+    internals: [{
+      name: 'Jack',
+      salary: 1300
+    }]
+  }
+}; 
